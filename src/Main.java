18850d72df736394dc4f8f089381494faaa02d60
Kirill K <francesc12008@gmail.com>
2022-09-15 17:13:32 +0300

    123

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,25 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Person firstPerson = new Person("Иванов Иван Иванович");
-        Person secondPerson = new Person("Петров Петр Петрович");
-        Person thirdPerson = new Person("Васильев Василий Васильевич");
+        Person firstPerson = new Person("Иванов Андрей", "начало древа", 12);
+        Person secondPerson = new Person("Иванов Евгений", "отец", 34);
+        Person thirdPerson = new Person("Васильева Алла", "мать", 30);
+        Person fourthPerson = new Person("Васильев Егор", "дедушка", 56);
 
         firstPerson.appendToFamily(secondPerson);
         firstPerson.appendToFamily(thirdPerson);
+        firstPerson.appendToFamily(fourthPerson);
 
         view(firstPerson);
+
     }
 
     static void view(Person rootPerson){
-        System.out.println(rootPerson.fullName);
-        for(Person person : rootPerson.getFamily()){
+        System.out.print("Имя:" + rootPerson.fullName);
+        System.out.print(". Статус: " + rootPerson.status);
+        System.out.println(". Возраст: " + rootPerson.age + ".");
+        for (Person person : rootPerson.getFamily()){
             if (person != null) {
                 view(person);
             }
--- src/Person.java
+++ src/Person.java
@@ -2,10 +2,14 @@ import java.util.ArrayList;
 
 public class Person {
     public String fullName;
+    public String status;
+    public int age;
     private ArrayList<Person> family = new ArrayList<>();
 
-    public Person(String fullName) {
+    public Person(String fullName, String status, int age) {
         this.fullName = fullName;
+        this.status = status;
+        this.age = age;
     }
 
     public void appendToFamily(Person person){
@@ -15,4 +19,13 @@ public class Person {
     public ArrayList<Person> getFamily() {
         return family;
     }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
 }
